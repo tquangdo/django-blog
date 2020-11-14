d70ba0e13a7c05b98f465c83d59808375d5fe799
tquangdo <trangia61@gmail.com>
2020-11-15 00:43:11 +0900

    20201115

--- PythonWeb/settings.py
+++ PythonWeb/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'xxx'
 # SECURITY WARNING: don't run with debug turned on in production!
 # xử lí 404!!!
 DEBUG = False
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
